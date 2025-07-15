eea18a078cab64bd02ce0a1e740093979f5c5d87
moon <msj7191@naver.com>
2025-07-15 10:07:14 +0900

    practice: 배열,집합 연습

--- array-basic.ts
+++ array-basic.ts
@@ -1,1 +1,14 @@
-console.log("테스트입니다");
+const arr = [1, 2, 3];
+arr.push(4);      // 맨 뒤 삽입
+arr.unshift(0);   // 맨 앞 삽입
+arr.pop();        // 맨 뒤 삭제
+arr.shift();      // 맨 앞 삭제
+console.log(arr);
+
+// set-basic.ts
+const set = new Set([1, 2, 3]);
+set.add(2);      // 중복은 추가 안 됨
+set.add(4);
+set.delete(1);
+console.log(set.has(4)); // true
+console.log(set);
